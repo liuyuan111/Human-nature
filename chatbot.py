da8e6f05001f3af48093067de92c1f00828d5458
liuyuan111 <2386023698@qq.com>
2019-05-09 18:18:05 +0800

    second

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,1 @@
-while True:print(input("").split(',')[-1].replace('吗','').replace("?","!"))
+while True:print(input("").split(',')[-1].replace('吗','').replace("?","!").replace('我','你',1).replace('你','我',1))
